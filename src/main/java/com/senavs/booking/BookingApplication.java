07d866ad4f290d8728dd0ef87389c20e715e952e
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2024-01-21 09:20:47 -0300

    chore: project initilizr

---
+++ src/main/java/com/senavs/booking/BookingApplication.java
@@ -0,0 +1,13 @@
+package com.senavs.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+}
